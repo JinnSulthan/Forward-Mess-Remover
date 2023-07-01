0adcc916dd1ba1f06bba5f5a66993b037b6bea75
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2023-07-01 21:30:37 +0530

    commit 2023(#1)

--- service.py
+++ service.py
@@ -1,30 +1,37 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User, Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-Client = Client(
+bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+
+app = Client(
     "Forward-Mess-Remover",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token=bot_token,
+    api_id=api_id,
+    api_hash=api_hash
 )
 
 START_BUTTON = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('SOURCE CODE', url="https://github.com/SpamShield/Forward-Mess-Remover")
-        ]]
-    ) 
+    [
+        [
+            InlineKeyboardButton('SOURCE CODE', url="https://github.com/SpamShield/Forward-Mess-Remover")
+        ]
+    ]
+)
 
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, message):
+@app.on_message(filters.private & filters.command("start"))
+async def start(_, message):
     await message.reply_sticker("CAACAgUAAxkBAAEBcr1hsLH3Nu0-qQpwwWQ7FkF58xnwSgACpAMAAjieoFU-Q-udLfwBUx4E")
     await message.reply_text(
-        f""" Hai {message.from_user.mention} am Forward-Mess-Remover Bot.""", 
+        f"Hai {message.from_user.mention}, I am Forward-Mess-Remover Bot.",
         disable_web_page_preview=True,
         reply_markup=START_BUTTON
     )
-@Client.on_message(filters.forwarded)
-async def delete(bot,message):
- await message.delete()
 
-Client.run()
+@app.on_message(filters.forwarded)
+async def delete(_, message):
+    await message.delete()
+
+app.run()
